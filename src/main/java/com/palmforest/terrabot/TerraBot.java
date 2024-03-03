8dab58494b6156314144da5761bf4b2f6fb42148
PALMFOREST\realp <realpalmforest@gmail.com>
2024-03-03 19:58:47 +1100

    Fixed token leak and added Dotenv config file containing the token

--- src/main/java/com/palmforest/terrabot/TerraBot.java
+++ src/main/java/com/palmforest/terrabot/TerraBot.java
@@ -1,5 +1,7 @@
 package com.palmforest.terrabot;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.exceptions.InvalidTokenException;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
 
@@ -7,11 +9,14 @@ import javax.security.auth.login.LoginException;
 
 public class TerraBot
 {
+    private final Dotenv config;
     private final ShardManager shardManager;
 
-    public TerraBot() throws LoginException
+    public TerraBot() throws InvalidTokenException
     {
-        String token = "MTE0MjMzMzc0OTIxNjk0NDIwOA.G1A-_J.4ffw6W98HFC0pwn-jMiT_g-paky7TN4UlhfFdM";
+        config = Dotenv.configure().load();
+        String token = config.get("TOKEN");
+
         DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
         shardManager = builder.build();
     }
@@ -19,14 +24,15 @@ public class TerraBot
     public ShardManager getShardManager() {
         return shardManager;
     }
+    public Dotenv getConfig() { return config; }
 
 
     public static void main(String[] args)
     {
         try {
             TerraBot bot = new TerraBot();
-        } catch (LoginException e) {
-            System.out.println("Failed to start bot. The bot token provided is invalid.");
+        } catch (InvalidTokenException e) {
+            System.out.println("Failed to start bot. The provided bot token is invalid.");
         }
     }
 }
